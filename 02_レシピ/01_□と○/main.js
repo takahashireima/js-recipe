3a5a8f0d35d4057822505f3a7e410ee4b3cb1e84
Githubreima <Githubreima17@icloud.com>
2023-07-22 00:53:42 +0900

    [□と○]を学習しました。

---
+++ 02_レシピ/01_□と○/main.js
@@ -0,0 +1,11 @@
+const figure = document.getElementById("figure")
+const squareButton = document.getElementById("square-button")
+const circleButton = document.getElementById("circle-button")
+
+circleButton.onclick = function () {
+  figure.classList.add("rounded")
+}
+
+squareButton.onclick = function () {
+  figure.classList.remove("rounded")
+}
