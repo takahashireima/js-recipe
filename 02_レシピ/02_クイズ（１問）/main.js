6c26f889cdc7ac242b0765956e3ef2a376e3870f
Githubreima <Githubreima17@icloud.com>
2023-08-03 21:33:22 +0900

    [クイズ (1問)]を学習しました。

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,52 +1,56 @@
-const choice1 = document.getElementById("choice-1")
-const choice2 = document.getElementById("choice-2")
-const choice3 = document.getElementById("choice-3")
+const Choices_Container = document.getElementById("choices-container")
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
 const feedback = document.getElementById("feedback")
 
 const quiz = {
-  text: "この星は何でしょう？",
-  image: "Ganymede.jpg",
+  text: "このアニメの主人公は誰？",
+  image: "singeki.png",
   choices: [
     {
-      text: "ゴリアテ",
-      feedback:
-        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+      text: "虎杖悠仁",
+      feedback: "残念！呪術廻戦の主人公だよ！",
     },
     {
-      text: "ゼニガメ",
-      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      text: "奴良リクオ",
+      feedback: "残念！ぬらりひょんの孫の主人公だよ！",
     },
     {
-      text: "ガニメデ",
-      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      text: "ゴン＝フリークス",
+      feedback: "残念！ハンターハンターの主人公だよ！",
+    },
+    {
+      text: "エレン・イェーガー",
+      feedback: "正解！早くアニメ来てほしいよね！映画かも。。？",
+    },
+    {
+      text: "五条",
+      feedback: "かっこよい。",
     },
   ],
 }
 
 const reloadQuiz = function () {
+  const array = []
+
   quizText.textContent = "Q, " + quiz.text
 
   quizImage.src = "./images/" + quiz.image
 
-  choice1.textContent = quiz.choices[0].text
-  choice2.textContent = quiz.choices[1].text
-  choice3.textContent = quiz.choices[2].text
+  for (let i = 0; i < quiz.choices.length; i++) {
+    const Button = document.createElement("button")
+    Button.setAttribute("id", "choice-" + (i + 1))
+    Choices_Container.appendChild(Button)
+    Button.textContent = quiz.choices[i].text
+    array.push(document.getElementById("choice-" + (i + 1)))
+    array[i].onclick = function () {
+      choose(i)
+    }
+  }
 }
 
 const choose = function (choiceNumber) {
   feedback.textContent = quiz.choices[choiceNumber].feedback
-}
-
-choice1.onclick = function () {
-  choose(0)
-}
-choice2.onclick = function () {
-  choose(1)
-}
-choice3.onclick = function () {
-  choose(2)
 }
 
 reloadQuiz()
